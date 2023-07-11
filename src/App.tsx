1eff3da6ef6a018115b18839e0da07d81769a148
Tom Lilley <tom@tomlilley.co.uk>
2023-07-11 15:20:51 +0700

    Add types for the countries api, add component to display list of countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Container } from '@/components/UI/container';
 import { SearchBox } from '@/components/UI/input';
 import { LargeHeading } from '@/components/UI/text';
-import Header from '@/header';
+import Header from '@/components/header';
 import useDarkMode from '@/hooks/useDarkMode';
+import data from '../data.json';
+import { Country } from '@/types';
+import CountryList from '@/views/countryList';
 
 function App() {
   const toggleDarkMode = useDarkMode();
+  const countriesList = data.countries as Country[];
   return (
     <>
       <Header toggleDarkMode={toggleDarkMode} />
       <Container>
         <LargeHeading>Hello World</LargeHeading>
         <SearchBox placeholder="Search for a country..." />
+        <CountryList countryList={countriesList} />
       </Container>
     </>
   );
---
+++ src/components/countryList.tsx
@@ -0,0 +1,29 @@
+import { CardWithImage } from '@/components/UI/card';
+import { Country } from '@/types';
+
+export default function CountryList({
+  countryList,
+}: {
+  countryList: Country[];
+}) {
+  return (
+    <div className="grid grid-cols-countries  items-stretch justify-around lg:justify-between gap-x-8 lg:gap-y-16 gap-y-10">
+      {countryList.map((country) => (
+        <button key={country.name} type="button">
+          <CardWithImage
+            image={country.flags.png}
+            name={country.name}
+            details={[
+              {
+                title: 'Population',
+                body: country.population.toLocaleString(),
+              },
+              { title: 'Region', body: country.region },
+              { title: 'Capital', body: country.capital || 'None' },
+            ]}
+          />
+        </button>
+      ))}
+    </div>
+  );
+}
